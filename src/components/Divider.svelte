382cdf926a7693fbab53fb0eb2fb8dffbd5232ac
Arexon <arexonreal@gmail.com>
2021-10-15 12:44:21 +0200

    :lipstick: Update Divider components

--- src/components/Divider.svelte
+++ src/components/Divider.svelte
@@ -2,17 +2,21 @@
 	export let small
 	export let medium
 	export let large
+	export let full
 </script>
 
 <hr
 	class="divider"
 	class:divider_small={small}
 	class:divider_medium={medium}
-	class:divider_large={large}/>
+	class:divider_large={large}
+	class:divider_full={full}
+/>
 
 <style lang="scss">
 	.divider {
 		margin: $space-2 auto;
+		width: 0;
 		height: 1.5px;
 		background: $color-neutral-2;
 
@@ -24,6 +28,9 @@
 		}
 		&_large {
 			width: 75%;
+		}
+		&_full {
+			width: 100%;
 		}
 	}
 </style>
