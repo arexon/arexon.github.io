86e82105f4cff0577760d2ccc14e283932423eb9
Arexon <arexonreal@gmail.com>
2021-10-15 23:41:23 +0200

    :sparkles: Add Tabs component

---
+++ src/components/Tabs.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { url } from '$lib/url'
+
+	export let tabs = []
+
+	const toSnakeCase = tab => tab.toLowerCase().split(' ').join('_')
+</script>
+
+<div class="tabs">
+	{#each tabs as tab}
+		<a class="tabs_item" class:active={$url.hash === `#${toSnakeCase(tab)}`} href={`#${toSnakeCase(tab)}`}>{tab}</a>
+	{/each}
+</div>
+
+<style lang="scss">
+	.tabs {
+		position: sticky;
+		top: $space-4;
+		display: flex;
+		justify-content: center;
+		background: $color-neutral-1;
+		transform: translateY(-$space-4);
+
+		&_item {
+			padding: $space-0 $space-1;
+			color: $color-neutral-3;
+			font-family: $font-secondary;
+			font-weight: 500;
+			text-transform: uppercase;
+			text-decoration: none !important;
+			&:hover {
+				color: $color-primary-2;
+			}
+
+			&.active {
+				color: $color-neutral-1;
+				background: $color-neutral-3;
+				&:hover {
+					background: $color-primary-2;
+				}
+			}
+		}
+	}
+</style>
