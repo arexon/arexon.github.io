3ff544841738a785f0b6e5eb28b1f6c6a6bcb846
Arexon <arexonreal@gmail.com>
2021-10-16 12:12:13 +0200

    :sparkles: Add FurniDeco project

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,9 +1,113 @@
 const projects = [
 	{
 		name: 'FurniDeco',
-		description: 'Adds 50 pieces of functional, decorative and unique furniture that fit with the Minecraft vanilla style.',
+		description: 'Adds 25 pieces of functional, decorative and unique furniture that fit with the Minecraft vanilla style.',
 		image: '/pages/furnideco/thumbnail.png',
-		link: '/project/furnideco'
+		link: '/project/furnideco',
+		recipes: {
+			rustic_grade: [
+				{
+					name: 'Book',
+					image: '/pages/furnideco/recipes/book.png'
+				},
+				{
+					name: 'Bottle',
+					image: '/pages/furnideco/recipes/bottle.png'
+				},
+				{
+					name: 'Cabinet',
+					image: '/pages/furnideco/recipes/cabinet.png'
+				},
+				{
+					name: 'Ceiling Fan',
+					image: '/pages/furnideco/recipes/ceiling_fan.png'
+				},
+				{
+					name: 'Chair',
+					image: '/pages/furnideco/recipes/chair.png'
+				},
+				{
+					name: 'Chandelier',
+					image: '/pages/furnideco/recipes/chandelier.png'
+				},
+				{
+					name: 'Clock',
+					image: '/pages/furnideco/recipes/clock.png'
+				},
+				{
+					name: 'Coffee Table',
+					image: '/pages/furnideco/recipes/coffee_table.png'
+				},
+				{
+					name: 'Curtain',
+					image: '/pages/furnideco/recipes/curtain.png'
+				},
+				{
+					name: 'Fire Pit',
+					image: '/pages/furnideco/recipes/fire_pit.png'
+				},
+				{
+					name: 'Grandfather Clock',
+					image: '/pages/furnideco/recipes/grandfather_clock.png'
+				},
+				{
+					name: 'Lamp',
+					image: '/pages/furnideco/recipes/lamp.png'
+				},
+				{
+					name: 'Mounted Goat Horn',
+					image: '/pages/furnideco/recipes/mounted_goat_horn.png'
+				},
+				{
+					name: 'Plant Pot',
+					image: '/pages/furnideco/recipes/plant_pot.png'
+				},
+				{
+					name: 'Radio',
+					image: '/pages/furnideco/recipes/radio.png'
+				},
+				{
+					name: 'Rocking Chair',
+					image: '/pages/furnideco/recipes/rocking_chair.png'
+				},
+				{
+					name: 'Scroll',
+					image: '/pages/furnideco/recipes/scroll.png'
+				},
+				{
+					name: 'Shelve',
+					image: '/pages/furnideco/recipes/shelve.png'
+				},
+				{
+					name: 'Sofa',
+					image: '/pages/furnideco/recipes/sofa.png'
+				},
+				{
+					name: 'Step Ladder',
+					image: '/pages/furnideco/recipes/step_ladder.png'
+				},
+				{
+					name: 'Stool',
+					image: '/pages/furnideco/recipes/stool.png'
+				},
+				{
+					name: 'Table',
+					image: '/pages/furnideco/recipes/table.png'
+				},
+				{
+					name: 'Television',
+					image: '/pages/furnideco/recipes/television.png'
+				},
+				{
+					name: 'Trash Can',
+					image: '/pages/furnideco/recipes/trash_can.png'
+				},
+				{
+					name: 'Vase',
+					image: '/pages/furnideco/recipes/vase.png'
+				}
+			]
+		}
 	}
 ]
 
--- src/routes/project/furnideco.svelte
+++ src/routes/project/furnideco.svelte
@@ -1,25 +1,138 @@
 <script>
 	import Head from '../../components/Head.svelte'
 	import Banner from '../../components/Banner.svelte'
+	import Tabs from '../../components/Tabs.svelte'
 	import Card from '../../components/Card.svelte'
+	import Button from '../../components/Button.svelte'
+	import Divider from '../../components/Divider.svelte'
 	import Footer from '../../components/Footer.svelte'
 	import { projects } from '$lib/projects'
+	import { url as getUrl } from '$lib/url'
 	let furnideco = projects[0]
 
 	let location = 'FurniDeco | Projects | Arexon'
 	let url = 'https://arexon.dev/project/furnideco'
-	let keywords = 'Arexon, Arexon add-ons, Arexon mods, FurniDeco mod, Fabric furniture, Minecraft furniture, Furniture add-ons, FurniDeco add-ons'
+	let keywords = 'Arexon, Arexon add-ons, Minecraft furniture, Furniture add-ons, FurniDeco add-ons'
 </script>
 
 <Head title={furnideco.name} description={furnideco.description} location={location} url={url} keywords={keywords} image={furnideco.image}/>
 
 <Banner title={furnideco.name} location={location} description={furnideco.description}/>
 
+<Tabs tabs={['About', 'Recipes', 'Releases']}/>
+
 <article class="wrapper">
-	<Card>
-		<h2>Coming Soon</h2>
-		<p>FurniDeco is under development. There is no set in stone date for when it's coming out yet, but I am hoping for a release sometime in Q4 of this year.</p>
-	</Card>
+	{#if $getUrl.hash === '' || $getUrl.hash === '#about'}
+
+		<h2>About</h2>
+
+		<Divider full/>
+
+		<img class="space" src="/pages/furnideco/thumbnail.png" alt="FurniDeco Thumbnail">
+
+		<Card>FurniDeco adds 25 pieces of functional, decorative and unique-looking furniture that fit with the vanilla style. With more than 80 variants in total, FurniDeco gives you endless options to furniture & decorate your builds!</Card>
+
+		<Divider/>
+
+		<h2>Features</h2>
+		<ul>
+			<li>💺 25 Unique pieces of furniture & decorations, up to 80+ variants</li>
+			<li>🎨 Fully customizable</li>
+			<li>🎮 Interactive and functional</li>
+			<li>🍦 Fits with the Minecraft Vanilla feel</li>
+			<li>🪓 Craftable in survival mode</li>
+			<li>🧊 Uses blocks</li>
+		</ul>
+
+		<Divider/>
+
+		<h2>In-game Shots</h2>
+		<img class="space" src="/pages/furnideco/showcase-1.png" alt="FurniDeco ingame showcase #1">
+		<img class="space" src="/pages/furnideco/showcase-2.png" alt="FurniDeco ingame showcase #2">
+
+		<Divider/>
+
+		<h2>Furniture Grades</h2>
+		<p>To keep different kinds of furniture organized, FurniDeco divids them into <strong>Furniture Grades</strong>, each grade houeses furniture of a single theme. Currently the add-on has only one grade, but eventaully it'll expand into multiple grades in future updates.</p>
+
+		<Divider/>
+
+		<h3>➔ The Rustic Grade</h3>
+		<img class="space" src="/pages/furnideco/rustic_grade.png" alt="Rustic Grade">
+		<Card>Rustic and country themed furniture with a cozy vintage feel.</Card>
+
+		<Divider/>
+
+		<h2>Interactivity and Functionality</h2>
+		<p>One of the major goals FurniDeco aims to achieve is creating furniture that's not only decorative, but is also usefull!</p>
+		<img class="space" src="/pages/furnideco/interactivity.gif" alt="Interactivity">
+
+		<Divider/>
+
+		<h2>Customization</h2>
+		<p>The Paint Brush lets you conveniently and instantly paint specific parts of a furniture piece.</p>
+		<img class="space" src="/pages/furnideco/paint_brush.gif" alt="Paint Brush">
+
+		<Divider/>
+
+		<h2>The Carpentry Table</h2>
+		<p>The Carpentry Table makes it super easy to obtain furniture in survival mode by just crafting it! All you have to do is pop in the materials required and voila, you've got yourself a neat Rustic Sofa!</p>
+		<img class="space" src="/pages/furnideco/carpentry_table.gif" alt="Carpentry Table">
+		<p>➔ To view a list of all crafting recipes, <a href="#recipes">click on this cool-looking link!</a></p>
+
+		<Divider/>
+
+		<h2>License</h2>
+		<Card>FurniDeco is licensed under the Apache License 2.0, you can read more about it <a href="https://github.com/Arexon/furnideco/blob/main/LICENSE">here</a></Card>
+
+	{:else if $getUrl.hash === '#recipes'}
+
+		<h2>Crafting Recipes</h2>
+
+		<Divider full/>
+
+		<h3>➔ Rustic Grade</h3>
+		<article class="recipes">
+			{#each furnideco.recipes.rustic_grade as recipe}
+				<div>
+					<h4>{recipe.name}</h4>
+					<img class="recipe_image" src={recipe.image} alt={recipe.name}/>
+				</div>
+			{/each}
+		</article>
+
+	{:else if $getUrl.hash === '#releases'}
+
+		<h2>Releases</h2>
+
+		<Divider full/>
+
+		<Card>Download the latest release of FurniDeco or view all versions.</Card>
+
+		<Divider/>
+
+		<Button primary href="https://github.com/Arexon/furnideco/releases/download/v1.0.0/furnideco-v1.0.0.mcaddon" title="Download the latest version">Download ➔</Button>
+		<Divider/>
+		<Button secondary href="https://github.com/Arexon/furnideco/releases" title="View all FurniDeco releases on GitHub">View all version on GitHub ➔</Button>
+
+	{/if}
 </article>
 
 <Footer/>
+
+<style lang="scss">
+	.recipes {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(175px, 1fr));
+		grid-auto-rows: auto;
+		align-content: start;
+		gap: $space-2;
+		@include tabletScreen {
+			margin: 0 $space-2;
+		}
+	}
+
+	.recipe_image {
+		background: $color-neutral-5;
+	}
+</style>
