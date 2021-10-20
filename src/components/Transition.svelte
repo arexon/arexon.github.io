4334153fcd815ed492f6e8f7b8f114870a0a01f3
Arexon <arexonreal@gmail.com>
2021-10-20 17:55:54 +0200

    :heavy_plus_sign: Add Transition component

---
+++ src/components/Transition.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { inview } from 'svelte-inview'
+
+	export let isInView
+	// let scrollDirection
+	const options = {
+		rootMargin: '-50px',
+		unobserveOnEnter: true,
+	}
+	const handleChange = ({ detail }) => {
+		isInView = detail.inView
+		scrollDirection = detail.scrollDirection.vertical
+	}
+</script>
+
+<div use:inview={options} on:change={handleChange}>
+	<slot/>
+</div>
