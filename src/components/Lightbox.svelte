d74b4f4d660d306b087a77d1079c98cac4c8f03a
Arexon <arexonreal@gmail.com>
2021-10-26 21:33:17 +0200

    :dizzy: Enhance Lightbox animations

--- src/components/Lightbox.svelte
+++ src/components/Lightbox.svelte
@@ -29,7 +29,7 @@
 		background: #160d0eee;
 		transition: transform .6s $curve-quart, opacity .4s $curve-quart;
 		opacity: 0;
-		transform: translateY(-100%);
+		transform: translateY(100%) scale(.75);
 
 		&_image {
 			border-radius: $radius-1;
@@ -37,7 +37,7 @@
 
 		&.show {
 			opacity: 1;
-			transform: translateY(0);
+			transform: translateY(0) scale(1);
 		}
 
 		@include tabletScreen {
